c11b66e208f3966d6c70dc2382dfab22c59e5f59
David <davidngacha01@gmail.com>
2023-09-14 15:35:13 +0300

    new file:   app/controllers/orders_controller.rb 	new file:   app/helpers/orders_helper.rb 	new file:   app/models/order.rb 	modified:   config/routes.rb 	new file:   db/migrate/20230914123457_create_orders.rb 	new file:   test/controllers/orders_controller_test.rb 	new file:   test/fixtures/orders.yml 	new file:   test/models/order_test.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :orders
   resources :customers
   resources :carts
   resources :cart_items
---
+++ db/migrate/20230914123457_create_orders.rb
@@ -0,0 +1,12 @@
+class CreateOrders < ActiveRecord::Migration[7.0]
+  def change
+    create_table :orders do |t|
+      t.float :amount
+      t.integer :status
+      t.string :cart_data
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
