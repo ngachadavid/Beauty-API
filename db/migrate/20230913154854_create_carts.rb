85eb4a267c3e36b83ace8355ddd1f006ea927295
David <davidngacha01@gmail.com>
2023-09-14 16:13:51 +0300

    modified:   db/migrate/20230913154854_create_carts.rb 	new file:   db/schema.rb

--- db/migrate/20230913154854_create_carts.rb
+++ db/migrate/20230913154854_create_carts.rb
@@ -1,7 +1,7 @@
 class CreateCarts < ActiveRecord::Migration[7.0]
   def change
     create_table :carts do |t|
-      t.integer :beauty_product_id
+      t.integer :product_id
       t.integer :quantity
       t.boolean :active
       t.references :user, null: false, foreign_key: true
---
+++ db/schema.rb
@@ -0,0 +1,79 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2023_09_14_125402) do
+  create_table "beauty_products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.float "price"
+    t.string "image_url"
+    t.string "category"
+    t.integer "qty"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_beauty_products_on_user_id"
+  end
+
+  create_table "cart_items", force: :cascade do |t|
+    t.integer "cart_id", null: false
+    t.integer "beauty_product_id", null: false
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["beauty_product_id"], name: "index_cart_items_on_beauty_product_id"
+    t.index ["cart_id"], name: "index_cart_items_on_cart_id"
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.integer "beauty_product_id"
+    t.integer "quantity"
+    t.boolean "active"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_carts_on_user_id"
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "phone"
+    t.text "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.float "amount"
+    t.integer "status"
+    t.string "cart_data"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "beauty_products", "users"
+  add_foreign_key "cart_items", "beauty_products"
+  add_foreign_key "cart_items", "carts"
+  add_foreign_key "carts", "users"
+  add_foreign_key "orders", "users"
+end
