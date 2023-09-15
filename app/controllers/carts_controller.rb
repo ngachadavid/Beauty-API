ab7343672fa94e7448cc2f7e30c3d3932acec66f
David <davidngacha01@gmail.com>
2023-09-15 18:17:55 +0300

    fix: add carts controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,25 @@
 class CartsController < ApplicationController
-end
+   
+    def create
+      product = BeautyProduct.find(params[:id])
+      quantity = params[:quantity] || 1
+      cart_item = CartItem.new(product: product, quantity: quantity)
+      if cart_item.save
+        render json: { message: "Item added to cart" }, status: :created
+      else
+        render json: { errors: cart_item.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+  
+    def index
+      cart_items = CartItem.all
+      render json: cart_items
+    end
+  
+    def destroy
+      cart_item = CartItem.find(params[:id])
+      cart_item.destroy
+      head :no_content
+    end
+   end
+  
